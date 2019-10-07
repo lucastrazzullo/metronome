16dc1d01de24b267afc0caef73db1bdc32b1b283
Luca Strazzullo <lucastrazzullo86@gmail.com>
2019-10-07 22:38:53 +0200

    Supporting dark mode

--- Metronome/Metronome iOS/Classes/View/HelpView.swift
+++ Metronome/Metronome iOS/Classes/View/HelpView.swift
@@ -13,18 +13,15 @@ struct HelpView: View {
     var model: HelpViewModel
 
     var body: some View {        
-        ZStack {
-            Color.white.opacity(0.9).edgesIgnoringSafeArea(.all)
-            VStack(alignment: .center, spacing: 40) {
-                Text(model.titleLabel).font(Font.system(.title))
-                HStack(alignment: .top, spacing: 60) {
-                    ForEach(model.tips, id: \.self) { tipViewModel in
-                        VStack(alignment: .center, spacing: 40) {
-                            Image(tipViewModel.illustration).frame(width: 90, height: 90, alignment: .center)
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text(tipViewModel.title).multilineTextAlignment(.leading).font(Font.system(.headline))
-                                Text(tipViewModel.description).multilineTextAlignment(.leading).font(Font.system(.callout))
-                            }
+        VStack(alignment: .center, spacing: 40) {
+            Text(model.titleLabel).font(Font.system(.title))
+            HStack(alignment: .top, spacing: 60) {
+                ForEach(model.tips, id: \.self) { tipViewModel in
+                    VStack(alignment: .center, spacing: 40) {
+                        Image(tipViewModel.illustration).frame(width: 90, height: 90, alignment: .center)
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(tipViewModel.title).multilineTextAlignment(.leading).font(Font.system(.headline))
+                            Text(tipViewModel.description).multilineTextAlignment(.leading).font(Font.system(.callout))
                         }
                     }
                 }
