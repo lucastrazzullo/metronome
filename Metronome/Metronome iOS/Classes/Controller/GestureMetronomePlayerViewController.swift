33629ea19f1d791ad70416e0890c7fddfa6fb16a
Luca Strazzullo <lucastrazzullo86@gmail.com>
2019-10-08 20:52:34 +0200

    Cleanup

--- Metronome/Metronome iOS/Classes/Controller/GestureMetronomePlayerViewController.swift
+++ Metronome/Metronome iOS/Classes/Controller/GestureMetronomePlayerViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GestureMetronomePlayerViewController: UIViewController, ContainerViewController {
 
-    let metronome: Metronome
+    private let metronome: Metronome
 
     private var tempoUpdaterViewController: TempoUpdaterViewController?
     private var timeSignatureUpdaterViewController: TimeSignatureUpdaterViewController?
