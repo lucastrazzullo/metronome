78fab4f40eba9b5393eff5d4492a4cef527bc5c7
Luca Strazzullo <lucastrazzullo86@gmail.com>
2019-10-13 15:02:52 +0200

    Added new gesture for tap tempo in helper

--- Metronome/Metronome iOS/Classes/View/HelpView.swift
+++ Metronome/Metronome iOS/Classes/View/HelpView.swift
@@ -12,20 +12,32 @@ struct HelpView: View {
 
     var model: HelpViewModel
 
-    var body: some View {        
-        VStack(alignment: .center, spacing: 40) {
-            Text(model.titleLabel).font(Font.system(.title))
-            HStack(alignment: .top, spacing: 60) {
-                ForEach(model.tips, id: \.self) { tipViewModel in
-                    VStack(alignment: .center, spacing: 40) {
-                        Image(tipViewModel.illustration).frame(width: 90, height: 90, alignment: .center)
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text(tipViewModel.title).multilineTextAlignment(.leading).font(Font.system(.headline))
-                            Text(tipViewModel.description).multilineTextAlignment(.leading).font(Font.system(.callout))
-                        }
-                    }
+
+    // MARK: Body
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .center, spacing: 40) {
+                Text(self.model.titleLabel).font(Font.system(.title))
+                HStack(alignment: .top, spacing: 40) {
+                    ForEach(self.model.tips, id: \.self) { tipViewModel in
+                        VStack(alignment: .center, spacing: CGFloat(self.model.spacingBetweenTips)) {
+                            Image(tipViewModel.illustration).frame(width: 90, height: 90, alignment: .center)
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text(tipViewModel.title).multilineTextAlignment(.leading).font(Font.system(.headline))
+                                Text(tipViewModel.description).multilineTextAlignment(.leading).font(Font.system(.callout))
+                            }
+                        }.frame(width: self.width(for: tipViewModel, geometry: geometry), height: nil, alignment: .center)
+                    }.animation(.spring())
                 }
             }
         }
+    }
+
+
+    // MARK: Private helper methods
+
+    private func width(for tipViewModel: TipViewModel, geometry: GeometryProxy) -> CGFloat {
+        return (geometry.size.width / CGFloat(model.numberOfVisibleTips)) - CGFloat(model.spacingBetweenTips)
     }
 }
