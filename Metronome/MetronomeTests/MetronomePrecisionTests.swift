c44c9824e6721ede0f77fc44328fc609986b252f
Luca Strazzullo <lucastrazzullo86@gmail.com>
2019-10-20 22:06:52 +0200

    Added test for precision in one minute - the test is disabled, before running the test, it needs to be enabled in the iOS app schema

---
+++ Metronome/MetronomeTests/MetronomePrecisionTests.swift
@@ -0,0 +1,65 @@
+//
+//  MetronomePrecisionTests.swift
+//  MetronomeTests
+//
+//  Created by luca strazzullo on 20/10/19.
+//  Copyright © 2019 luca strazzullo. All rights reserved.
+//
+
+import XCTest
+
+class MetronomePrecisionTests: XCTestCase {
+
+    private var tickExpectation: XCTestExpectation?
+    private var metronome: Metronome?
+
+
+    // MARK: Test life cycle
+
+    override func setUp() {
+        super.setUp()
+        let configuration = MetronomeConfiguration(timeSignature: TimeSignature.default, tempo: Tempo.default)
+        metronome = Metronome(with: configuration)
+        metronome?.delegate = self
+    }
+
+
+    override func tearDown() {
+        super.tearDown()
+        metronome?.reset()
+    }
+
+
+    // MARK: Tests
+
+    func testOneMinute() {
+        tickExpectation = expectation(description: "one-minute")
+        tickExpectation?.expectedFulfillmentCount = 120
+
+        metronome?.configuration.setBpm(120)
+        metronome?.configuration.setNotLength(.quarter)
+        metronome?.start()
+
+        wait(for: [tickExpectation!], timeout: 60)
+    }
+}
+
+
+extension MetronomePrecisionTests: MetronomeDelegate {
+
+    func metronome(_ metronome: Metronome, didUpdate configuration: MetronomeConfiguration) {
+    }
+
+
+    func metronome(_ metronome: Metronome, didPulse beat: Beat) {
+        tickExpectation?.fulfill()
+    }
+
+
+    func metronome(_ metronome: Metronome, willStartWithSuspended beat: Beat?) {
+    }
+
+
+    func metronome(_ metronome: Metronome, willResetDuring beat: Beat?) {
+    }
+}
