6a6e2ce6f9bf929f8fd4ece8ea48b12b26cb08ab
Luca Strazzullo <lucastrazzullo86@gmail.com>
2020-05-08 17:11:09 +0200

    Reset the metronome when the configuration changes

--- Metronome/Metronome/Application/Metronome.swift
+++ Metronome/Metronome/Application/Metronome.swift
@@ -24,6 +24,7 @@ class Metronome {
 
     var configuration: MetronomeConfiguration {
         didSet {
+            reset()
             delegate?.metronome(self, didUpdate: configuration)
         }
     }
