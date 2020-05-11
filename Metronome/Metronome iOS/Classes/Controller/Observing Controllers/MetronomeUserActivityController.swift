50f5d2442f2ca79699f1e68ce3f44e5004fea2b8
Luca Strazzullo <lucastrazzullo86@gmail.com>
2020-05-11 16:00:27 +0200

    Implemented handoff between iPad and iPhone

--- Metronome/Metronome iOS/Classes/Controller/Observing Controllers/MetronomeUserActivityController.swift
+++ Metronome/Metronome iOS/Classes/Controller/Observing Controllers/MetronomeUserActivityController.swift
@@ -9,12 +9,22 @@
 import Foundation
 import Combine
 
-class MetronomeUserActivityController: ObservingController {
+class MetronomeUserActivityController: NSObject, ObservingController {
 
     // MARK: Instance properties
 
     private var startMetronomeActivity: NSUserActivity?
     private var cancellables: [AnyCancellable] = []
+
+    private let metronome: Metronome
+
+
+    // MARK: Object life cycle
+
+    init(metronome: Metronome) {
+        self.metronome = metronome
+        super.init()
+    }
 
 
     // MARK: Public methods
@@ -22,11 +32,20 @@ class MetronomeUserActivityController: ObservingController {
     func set(publisher: MetronomePublisher) {
         cancellables.append(publisher.$configuration.sink { [weak self] configuration in
             self?.startMetronomeActivity = UserActivityFactory.buildStartMetronomeActivity(for: configuration)
+            self?.startMetronomeActivity?.delegate = self
         })
 
         cancellables.append(publisher.$isRunning.sink { [weak self] isRunning in
             if isRunning { self?.startMetronomeActivity?.becomeCurrent() }
             else { self?.startMetronomeActivity?.resignCurrent() }
         })
+    }
+}
+
+
+extension MetronomeUserActivityController: NSUserActivityDelegate {
+
+    func userActivityWasContinued(_ userActivity: NSUserActivity) {
+        metronome.reset()
     }
 }
